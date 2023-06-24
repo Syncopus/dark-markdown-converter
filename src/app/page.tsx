9022437dd1716e8ff8b00a538c82fb3ae6aed95e
Syncopus <jules.chevallet@gmail.com>
2023-06-24 15:06:44 +0200

    Fix synchronous script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import styles from './page.module.css'
 import dynamic from 'next/dynamic';
+import Script from 'next/script'
 
 const DynamicComponent = dynamic(() => import('./DynamicComponent'), {
   ssr: false, // Disable server-side rendering for this component
@@ -9,6 +10,7 @@ const DynamicComponent = dynamic(() => import('./DynamicComponent'), {
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></Script>
       <DynamicComponent/>
     </main>
   )
